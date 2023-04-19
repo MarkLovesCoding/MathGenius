28e6465f5d63a12510a5a3e0aa0ec5e8dd26eccd
Mark Halstead <mark.d.halstead@gmail.com>
2023-04-19 15:54:55 -0600

    Put Routes in Seperate file

--- public/routes/routes.js
+++ public/routes/routes.js
@@ -1,6 +1,81 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const path = require('path');
+
+
+
+const requireAuth = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    // redirect to login page if not authenticated
+    return res.redirect('/login');
+  }
+  next();
+};
+
+// set up the home route
+router.get('/', requireAuth, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/views/login.html'));
+});
+
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
+
+router.get('/play', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+router.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/views/signup.html'));
+});
+
+
+
+
+
+
+
+// router.get('/login', function(req, res, next) {
+//   res.render('login', { title: 'Login' });
+// });
+
+// router.post('/login', passport.authenticate('local', {
+//   successRedirect: '/profile',
+//   failureRedirect: '/login',
+//   failureFlash: true
+// }));
+
+// router.get('/signup', function(req, res, next) {
+//   res.render('signup', { title: 'Sign Up' });
+// });
+
+router.post('/signup', function(req, res, next) {
+  const { firstName, lastName, email, password } = req.body;
+  const user = new User({ firstName, lastName, email, password });
+  user.save(function(err) {
+    if (err) { return next(err); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/profile');
+    });
+  });
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+// router.get('/profile', isLoggedIn, function(req, res){
+//   res.render('profile', { title: 'Profile', user: req.user });
+// });
+
+
+
 
 // route for Google authentication
 router.get(
@@ -27,3 +102,5 @@ router.post(
 );
 
 module.exports = router;
+
+
--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const passport = require('passport')
-const session = require('express-session')
+const passport = require('passport');
+const session = require('express-session');
 const routes = require('./routes/routes');
-const passportConfig = require('./config/passport-config'); // import the passport configuration
+const passportConfig = require('./config/passport-config');
 const db = require('./db/db');
 
-
 const app = express();
-
 
 // set up the logger middleware
 app.use(morgan('dev'));
@@ -20,68 +18,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-
- app.use(session({
+app.use(session({
   secret: 'my-secret-key-2lkj2n3ifulsidfnlkj3r23r232nkiiiicc7s8s873hdkn4k',
   resave: false,
   saveUninitialized: false,
 }));
 
-app.use(passport.initialize()) 
- app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
- passportConfig(passport);
+passportConfig(passport);
 
-const requireAuth = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    // redirect to login page if not authenticated
-    return res.redirect('/login');
-  }
-  next();
-}
 app.use('/', routes);
 
-
-app.get('/', requireAuth, (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-// 
-
-// app.get('/', (req, res) => {
-//   db.collection('myCollection').find().toArray((err, data) => {
-//     if (err) throw err;
-//     res.send(data);
-//   });
-// });
-// 
-
-
-
-
-
-// set up the home route
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/views/login.html'));
-});
-
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
-
-app.get('/play', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/views/signup.html'));
-});
-
-
-
 // serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'dist'))  );
-
-
-
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // start the server
 const port = 4000;
