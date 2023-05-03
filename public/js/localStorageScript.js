343633aa6e2193636cb1d0c126f47a46866accbe
Mark Halstead <mark.d.halstead@gmail.com>
2023-05-03 16:00:36 -0600

    show localStorage

--- public/js/localStorageScript.js
+++ public/js/localStorageScript.js
@@ -1,8 +1,17 @@
 // Retrieve the userData from the data attribute of the script tag
 const sessionId = JSON.parse(document.currentScript.getAttribute('data-sessionId'));
+const userData = JSON.parse(document.currentScript.getAttribute('data-userData'));
 
 // Store the userData in localStorage
-localStorage.setItem('sessionId', JSON.stringify({
-  sessionId:sessionId.toString()
+localStorage.setItem('sessionId', JSON.stringify(
+  sessionId
   // Add any other properties you want to store
-}));
+))
+localStorage.setItem('userData', JSON.stringify({
+    userData:userData}
+    // Add any other properties you want to store
+  ))
+  
+
+
+;
