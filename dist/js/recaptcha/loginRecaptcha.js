23c89cfa1ce65d6e978882a6abd8cc15ada088d4
Mark Halstead <mark.d.halstead@gmail.com>
2023-05-21 15:46:34 -0600

    Update

--- dist/js/recaptcha/loginRecaptcha.js
+++
@@ -1,9 +0,0 @@
-function onSubmit(token) {
-  document.getElementById("login-form").submit();
-}
-function onSubmitGoogle(token) {
-  document.getElementById("google-login-form").submit();
-}
-function onSubmitGuest(token) {
-  document.getElementById("guest-login-form").submit();
-}
