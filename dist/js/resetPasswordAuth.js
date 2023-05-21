f0cbea54772031474d6a939c32ca174e6a1daa45
Mark Halstead <mark.d.halstead@gmail.com>
2023-05-21 14:17:05 -0600

    Update

---
+++ dist/js/resetPasswordAuth.js
@@ -0,0 +1,20 @@
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const submitBtn = document.querySelector('#reset-password-button');
+function validatePassword() {
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.setCustomValidity('Passwords do not match');
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+}
+password.addEventListener('change', validatePassword);
+confirmPassword.addEventListener('keyup', validatePassword);
+submitBtn.addEventListener('click', event => {
+  if (password.value !== confirmPassword.value) {
+    event.preventDefault();
+    confirmPassword.setCustomValidity('Passwords do not match');
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+});
