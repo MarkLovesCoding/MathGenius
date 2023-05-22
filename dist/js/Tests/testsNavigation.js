ca01286b02feb9ce0cabb343c3b6d3ae7e30fc90
Mark Halstead <mark.d.halstead@gmail.com>
2023-05-22 15:10:17 -0600

    Preload

---
+++ dist/js/Tests/testsNavigation.js
@@ -0,0 +1,33 @@
+import { state } from '../state.js';
+import { changeViewRight } from '../utils.js';
+const operatorContainer = document.getElementById("operator-menu-container");
+const activityContainer = document.getElementById("real-menu-container");
+const linkToGame = document.getElementById('new-game');
+linkToGame.addEventListener("click", function () {
+  // navigate to a different page
+
+  state.activity = "game";
+  // window.location.href = "/tests";
+  changeViewRight(activityContainer, operatorContainer);
+  // loadSection('operator-menu')
+});
+
+const linkToQuiz = document.getElementById('new-quiz');
+linkToQuiz.addEventListener("click", function () {
+  state.activity = "quiz";
+  changeViewRight(activityContainer, operatorContainer);
+
+  // window.location.href = "/practice";
+  // utilMethods.loadSection('practice-menu')
+  // loadSection('operator-menu')
+});
+
+const linkToMCQuiz = document.getElementById('new-mc-quiz');
+linkToMCQuiz.addEventListener("click", function () {
+  state.activity = "multiple-choice-quiz";
+  console.log(state.activeDifficulty);
+  // window.location.href = "/practice";
+  changeViewRight(activityContainer, operatorContainer);
+
+  // loadSection('operator-menu')
+});
