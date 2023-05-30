881c022a17aeab02e98080335a970a84e599ca24
Mark Halstead <mark.d.halstead@gmail.com>
2023-05-30 14:26:51 -0600

    Update Test

--- dist/js/learningNavigation.js
+++ dist/js/learningNavigation.js
@@ -1,1 +1,5 @@
-const linkToTT=document.getElementById("new-times-tables");linkToTT.addEventListener("click",function(){console.log(window.location),window.location.href="/learning/times-table"});
+const linkToTT = document.getElementById('new-times-tables');
+linkToTT.addEventListener("click", function () {
+  console.log(window.location);
+  window.location.href = "/learning/times-table";
+});
--- dist/js/resetPasswordAlert.js
+++ dist/js/resetPasswordAlert.js
@@ -1,1 +1,18 @@
-var resetPasswordAlert=document.getElementById("reset-password-flash-message");function closeAlert(){resetPasswordAlert.style.display="none"}window.onclick=function(e){e.target!==resetPasswordAlert&&(resetPasswordAlert.style.display="none")};
+var resetPasswordAlert = document.getElementById("reset-password-flash-message");
+
+// When the user clicks the button, open the modal 
+
+// When the user clicks on <span> (x), close the modal
+function closeAlert() {
+  resetPasswordAlert.style.display = 'none';
+}
+
+// var closeButton = document.getElementById('close-forgot-password-alert');
+// closeButton.addEventListener('click', closeAlert);
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target !== resetPasswordAlert) {
+    resetPasswordAlert.style.display = "none";
+  }
+};
