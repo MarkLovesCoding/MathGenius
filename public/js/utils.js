170e73662eb58c5726cef91d0e56668b1c6dc97b
Mark Halstead <mark.d.halstead@gmail.com>
2023-05-30 12:10:58 -0600

    Style Update

--- public/js/utils.js
+++ public/js/utils.js
@@ -305,8 +305,13 @@ export function convertStringToArray(inputString) {
   export function changeViewLeft(element1,element2){
     element1.style.left = 0;
     element2.style.left = "100%";
+    element1.style.opacity = 1;
+    element2.style.opacity = 0;
+
   }
   export function changeViewRight(element1,element2){
     element1.style.left = "-100%";
     element2.style.left = 0;
+    element1.style.opacity = 0;
+    element2.style.opacity = 1;
   }
