897072600c5f31315ef6eaf709044466a52aa3ce
Mark Halstead <mark.d.halstead@gmail.com>
2023-05-30 11:24:28 -0600

    First

--- dist/js/signupAlert.js
+++ dist/js/signupAlert.js
@@ -1,1 +1,19 @@
-var signupAlert=document.getElementById("signup-flash-message");function closeAlert(){signupAlert.style.display="none"}var closeButton=document.getElementById("close-signup-alert");closeButton&&closeButton.addEventListener("click",closeAlert),window.onclick=function(e){e.target!==signupAlert&&(signupAlert.style.display="none")};
+var signupAlert = document.getElementById("signup-flash-message");
+
+// When the user clicks the button, open the modal 
+
+// When the user clicks on <span> (x), close the modal
+function closeAlert() {
+  signupAlert.style.display = 'none';
+}
+var closeButton = document.getElementById('close-signup-alert');
+if (closeButton) {
+  closeButton.addEventListener('click', closeAlert);
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target !== signupAlert) {
+    signupAlert.style.display = "none";
+  }
+};
