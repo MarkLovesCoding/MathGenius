08fcb448aba67ecd6d1510a0c8d31824ec9e44aa
Mark Halstead <mark.d.halstead@gmail.com>
2023-06-01 10:53:42 -0600

    Add Fractions Skeleton. Add Timeout Timestables

--- public/js/timesTablesScript.js
+++ public/js/timesTablesScript.js
@@ -7,62 +7,66 @@ window.addEventListener("resize", function () {
 });
 
 window.addEventListener("DOMContentLoaded", function () {
-  var numbers = document.querySelectorAll(".number");
-  var timesTable = document.querySelector(".times-table");
-  var activeNumber =  document.querySelector('.number[data-number="1"]');
-  
-
-      numbers.forEach(function (number) {
-    number.addEventListener("mouseenter", function () {
-      
-  if(isLandscape){
-      
-      var numberValue = parseInt(number.getAttribute("data-number"));
-
-      if (activeNumber !== null) {
-        activeNumber.classList.remove("active");
-        // timesTable.style.width = "0";
-  
-          timesTable.innerHTML = generateTimesTable(numberValue);
-          // timesTable.style.width = "300px"
+  setTimeout(function(){
+    var numbers = document.querySelectorAll(".number");
+    var timesTable = document.querySelector(".times-table");
+    var activeNumber =  document.querySelector('.number[data-number="1"]');
     
-      } else {
-        timesTable.innerHTML = generateTimesTable(numberValue);
-        // timesTable.style.width = "300px";
-      }
-
-      number.classList.add("active");
-      activeNumber = number;
+      // Load times table for number 1 on page load
+      var initialNumber = document.querySelector('.number[data-number="1"]');
+      initialNumber.classList.add("active");
+      timesTable.innerHTML = generateTimesTable(1);
+      timesTable.style.height = timesTable.scrollHeight + "px";
+        numbers.forEach(function (number) {
+      number.addEventListener("mouseenter", function () {
+        
+    if(isLandscape){
+        
+        var numberValue = parseInt(number.getAttribute("data-number"));
+  
+        if (activeNumber !== null) {
+          activeNumber.classList.remove("active");
+          // timesTable.style.width = "0";
+    
+            timesTable.innerHTML = generateTimesTable(numberValue);
+            // timesTable.style.width = "300px"
       
-      
-  }
-  else{
-          var numberValue = parseInt(number.getAttribute("data-number"));
-
-      if (activeNumber !== null) {
-        activeNumber.classList.remove("active");
-        // timesTable.style.height = "0";
-        // setTimeout(function () {
+        } else {
+          timesTable.innerHTML = generateTimesTable(numberValue);
+          // timesTable.style.width = "300px";
+        }
+  
+        number.classList.add("active");
+        activeNumber = number;
+        
+        
+    }
+    else{
+            var numberValue = parseInt(number.getAttribute("data-number"));
+  
+        if (activeNumber !== null) {
+          activeNumber.classList.remove("active");
+          // timesTable.style.height = "0";
+          // setTimeout(function () {
+            timesTable.innerHTML = generateTimesTable(numberValue);
+            // timesTable.style.height = timesTable.scrollHeight + "px";
+          // }, 300);
+        } else {
           timesTable.innerHTML = generateTimesTable(numberValue);
           // timesTable.style.height = timesTable.scrollHeight + "px";
-        // }, 300);
-      } else {
-        timesTable.innerHTML = generateTimesTable(numberValue);
-        // timesTable.style.height = timesTable.scrollHeight + "px";
-      }
-
-      number.classList.add("active");
-      activeNumber = number;
-  }    
-      
-    });
-      })
+        }
   
-  // Load times table for number 1 on page load
-  var initialNumber = document.querySelector('.number[data-number="1"]');
-  initialNumber.classList.add("active");
-  timesTable.innerHTML = generateTimesTable(1);
-  timesTable.style.height = timesTable.scrollHeight + "px";
+        number.classList.add("active");
+        activeNumber = number;
+    }    
+        
+      });
+        })
+    
+
+
+  },200)
+ 
 });
 
 function generateTimesTable(number) {
