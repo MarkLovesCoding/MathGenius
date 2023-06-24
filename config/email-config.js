fd68a1ddcb63b0afe1b87a69b280f2df24ef5eaf
Mark Halstead <mark.d.halstead@gmail.com>
2023-06-24 16:39:45 -0600

    Update Reset Pass
    add

--- config/email-config.js
+++ config/email-config.js
@@ -1,9 +1,13 @@
+require('dotenv').config({ path: '../environmental/mg/.env' });
+
 const SibApiV3Sdk = require('sib-api-v3-sdk');
+
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 // Configure API key authorization: api-key
 const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = "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";
+const sibAPI = process.env.SENDINBLUEAPI
+apiKey.apiKey = sibAPI
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
