2cedaf31cf6a86e0f6c19d739573cd845685a1a4
Mark Halstead <mark.d.halstead@gmail.com>
2023-10-19 13:17:42 -0600

    Build

--- dist/js/success.js
+++ dist/js/success.js
@@ -1,13 +1,1 @@
-var success = document.getElementById('success');
-// Fade out the overlay after 1 second
-if (success) {
-  setTimeout(function () {
-    success.style.opacity = "0";
-  }, 1000);
-
-  // Hide the overlay after 2 seconds
-  setTimeout(function () {
-    success.style.display = "none";
-    success.style.zIndex = "0";
-  }, 2000);
-}
+var success=document.getElementById("success");success&&(setTimeout(function(){success.style.opacity="0"},1e3),setTimeout(function(){success.style.display="none",success.style.zIndex="0"},2e3));
